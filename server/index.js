b1c30fb2ab250c75874f129706f5875a57152235
salina kuo <salina.kuo97@gmail.com>
2020-10-19 18:38:52 -0400

    phase 1-3

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer(async (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("I have items");
+})
+const port = 8081;
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
